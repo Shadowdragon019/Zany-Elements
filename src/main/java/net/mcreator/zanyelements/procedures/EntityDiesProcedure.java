e8955b49198d8f4e9e18dc72895785d68c905a60
Shadowdragon019019@gmail.com <45351052+wolfboycoolkid@users.noreply.github.com>
2020-12-01 18:07:02 -0600

    Fixed Totem not having openable GUI and added the Creeper Totem and some functionality

--- src/main/java/net/mcreator/zanyelements/procedures/EntityDiesProcedure.java
+++ src/main/java/net/mcreator/zanyelements/procedures/EntityDiesProcedure.java
@@ -1,12 +1,21 @@
 package net.mcreator.zanyelements.procedures;
 
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.Explosion;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.Minecraft;
 
+import net.mcreator.zanyelements.item.TotemOfExplosivesItem;
 import net.mcreator.zanyelements.ZanyelementsModElements;
 
 import java.util.Map;
@@ -20,6 +29,60 @@ public class EntityDiesProcedure extends ZanyelementsModElements.ModElement {
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure EntityDies!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure EntityDies!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(TotemOfExplosivesItem.block, (int) (1)).getItem())
+				|| (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+						.getItem() == new ItemStack(TotemOfExplosivesItem.block, (int) (1)).getItem()))) {
+			if ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+					.getItem() == new ItemStack(TotemOfExplosivesItem.block, (int) (1)).getItem())) {
+				if (world.getWorld().isRemote) {
+					Minecraft.getInstance().gameRenderer.displayItemActivation(
+							((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY));
+				}
+				(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)).shrink((int) 1);
+			} else if ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+					.getItem() == new ItemStack(TotemOfExplosivesItem.block, (int) (1)).getItem())) {
+				if (world.getWorld().isRemote) {
+					Minecraft.getInstance().gameRenderer.displayItemActivation(
+							((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY));
+				}
+				(((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY)).shrink((int) 1);
+			}
+			if (dependencies.get("event") != null) {
+				Object _obj = dependencies.get("event");
+				if (_obj instanceof Event) {
+					Event _evt = (Event) _obj;
+					if (_evt.isCancelable())
+						_evt.setCanceled(true);
+				}
+			}
+			if (world instanceof World && !world.getWorld().isRemote) {
+				world.getWorld().createExplosion(null, (int) (entity.getPosX()), (int) (entity.getPosY()), (int) (entity.getPosZ()), (float) 4,
+						Explosion.Mode.BREAK);
+			}
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).setHealth((float) 1);
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).clearActivePotions();
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.ABSORPTION, (int) 120, (int) 1));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 900, (int) 1));
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, (int) 800, (int) 0));
+		}
 	}
 
 	@SubscribeEvent
---
+++ src/main/java/net/mcreator/zanyelements/procedures/EntityTriesToGriefProcedure.java
@@ -0,0 +1,118 @@
+package net.mcreator.zanyelements.procedures;
+
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.EntityMobGriefingEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.zanyelements.item.TotemOfExplosivesItem;
+import net.mcreator.zanyelements.ZanyelementsModElements;
+
+import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Comparator;
+
+@ZanyelementsModElements.ModElement.Tag
+public class EntityTriesToGriefProcedure extends ZanyelementsModElements.ModElement {
+	public EntityTriesToGriefProcedure(ZanyelementsModElements instance) {
+		super(instance, 22);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure EntityTriesToGrief!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure EntityTriesToGrief!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure EntityTriesToGrief!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure EntityTriesToGrief!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure EntityTriesToGrief!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((entity instanceof CreeperEntity)) {
+			{
+				List<Entity> _entfound = world
+						.getEntitiesWithinAABB(Entity.class,
+								new AxisAlignedBB(x - (64 / 2d), y - (64 / 2d), z - (64 / 2d), x + (64 / 2d), y + (64 / 2d), z + (64 / 2d)), null)
+						.stream().sorted(new Object() {
+							Comparator<Entity> compareDistOf(double _x, double _y, double _z) {
+								return Comparator.comparing((Function<Entity, Double>) (_entcnd -> _entcnd.getDistanceSq(_x, _y, _z)));
+							}
+						}.compareDistOf(x, y, z)).collect(Collectors.toList());
+				for (Entity entityiterator : _entfound) {
+					{
+						AtomicReference<IItemHandler> _iitemhandlerref = new AtomicReference<>();
+						entityiterator.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+								.ifPresent(capability -> _iitemhandlerref.set(capability));
+						if (_iitemhandlerref.get() != null) {
+							for (int _idx = 0; _idx < _iitemhandlerref.get().getSlots(); _idx++) {
+								ItemStack itemstackiterator = _iitemhandlerref.get().getStackInSlot(_idx).copy();
+								if (((itemstackiterator).getItem() == new ItemStack(TotemOfExplosivesItem.block, (int) (1)).getItem())) {
+									if (dependencies.get("event") != null) {
+										Object _obj = dependencies.get("event");
+										if (_obj instanceof Event) {
+											Event _evt = (Event) _obj;
+											if (_evt.hasResult())
+												_evt.setResult(Event.Result.DENY);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityGrief(EntityMobGriefingEvent event) {
+		Entity entity = event.getEntity();
+		double i = entity.getPosX();
+		double j = entity.getPosY();
+		double k = entity.getPosZ();
+		World world = entity.world;
+		Map<String, Object> dependencies = new HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+}
